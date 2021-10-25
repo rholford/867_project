20d3359907e9ed4bdb938f8c02a1a9710fc7838a
rholford <robert.holford@live.com>
2021-10-24 21:13:46 -0400

    Data extraction into train/test from raw files completed.

--- utils.py
+++ utils.py
@@ -1,12 +1,63 @@
 #Preprocessing mockup (utils?)
 import csv
 import json
+import ast
+import random
+
+#Need keyword/vector?
+#GPU select?
+
+#read data method?
+#def...
 
 
 def ExtractData(datasetName):
-    if datasetName == ('reddit' or 'gab'):
-        conanFile = open ('./data/conan/CONAN.json', 'r')
-    
+    """
+    Returns training and testing dataset from dataset name ('gab, 'reddit', 'conan')
+    """
+    #training data percent
+    pct = 0.80
+    if datasetName in ['reddit', 'gab']:
+        dataFile = open ('./data/ohs/' + datasetName + '.csv', 'r', encoding='utf-8')
+        hateSpeechBlob = []
+        hsIdx = []
+        responseBlob = []
+        
+        reader = csv.DictReader(dataFile)
+        for row in reader:
+            x = row['text']
+            y = row['hate_speech_idx']
+            z = row['response']
+            if y == 'n/a':
+                continue
+            hateSpeechBlob.append(x)
+            hsIdx.append(y)
+            responseBlob.append(z)
+        #print(hateSpeechBlob[1])   
+        hateCount = 0
+        for item in hsIdx:
+            for i in item.strip('[]').split(', '):
+                hateCount += 1
+
+        hateSpeech, counterSpeech = [], []
+        lineNumber = 0
+        for hs, idx, cs in zip(hateSpeechBlob, hsIdx, responseBlob):
+            hs = hs.strip().split('\n')
+            for i in idx.strip('[]').split(', '):
+                try:
+                    hateSpeech.append('. '.join(hs[int(i) - 1].split('. ')[1:]).strip('\t'))
+                except:
+                    continue
+                    #Note this is because there is an error in the data that throws out of bounds
+                temp = []
+                for j in splitResponse(cs):
+                    if j.lower() == 'n/a':
+                        continue
+                    temp.append(j)
+                counterSpeech.append(temp)
+                lineNumber += 1
+                
+
     if datasetName == 'conan':
         dataFile = open ('./data/conan/CONAN.json', 'r')
         fileText = []
@@ -24,12 +75,55 @@ def ExtractData(datasetName):
         for item in enText:
             hateSpeech.append(item['hateSpeech'].strip())
             counterSpeech.append([item['counterSpeech'].strip()])
+        hateCount = len(hateSpeech)
+        dataFile.close()
+       
+    #print(hateSpeech)
+    #split the data randomly into train/test
+    randomIndex = []
+    for num in range(hateCount):
+        randomIndex.append(num) 
+    random.shuffle(randomIndex)
+    trainIndex = sorted(randomIndex[:int(pct*len(randomIndex))])
+    trainHate = []
+    for i in range(hateCount):
+        if (i in trainIndex):
+            trainHate.append(hateSpeech[i])
+    trainCounter = []
+    for i in range(hateCount):
+        if (i in trainIndex):
+            trainCounter.append(counterSpeech[i])
 
-        #todo: shuffle, split into test/train
-        return hateSpeech, counterSpeech
+    testHate = []
+    for i in range(hateCount):
+        if (i not in trainIndex):
+            testHate.append(hateSpeech[i])
+    testCounter = []
+    for i in range(hateCount):
+        if (i not in trainIndex):
+            testCounter.append(counterSpeech[i])
+    return trainHate, trainCounter, testHate, testCounter
 
-def main():
-    ExtractData('conan')
+#helper function for csvs
+def splitResponse(strResp):
+    result = ast.literal_eval(strResp)
+    #print(result)
+    retVal = []
+    for item in result:
+        retVal.append(item)
+    return retVal
 
+#context to response goes here
+#def...
+
+#model code goes here, see what we need
+#def///
+
+#def main():
+    #a, b = ExtractData('conan')
+    #a, b = ExtractData('reddit')
+    #a,b,c,d = ExtractData('gab')
+    #print(a[3])
+    #print(b[3])
 if __name__ == "__main__":
     main()
